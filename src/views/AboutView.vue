45a4516aa7167c2394c27b5478e04e3604c84df3
Emma Huang <huiwenhuang1998@gmail.com>
2024-06-19 15:42:23 +0800

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <CardOne></CardOne>
   </div>
 </template>
+
+<script>
+import CardOne from '@/components/CardOne.vue'
+
+export default {
+  components: {
+    CardOne
+  }
+}
+</script>
